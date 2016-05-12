f1793043e15c507615ff9c72bc0d070ad36b53a4
Kendrick Parks <kparks@Kendricks-MBP.PK5001Z>
2016-05-11 23:35:00 -0700

    get close to lesson 10 version of game

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace FPSLearning
+{
+	public class Shoot : MonoBehaviour
+	{
+
+		private RaycastHit hit;
+		private float nextFire;
+		private float rate = 0.3f;
+		private float range = 300;
+
+		// Use this for initialization
+		void Start ()
+		{
+		
+		}
+		
+		// Update is called once per frame
+		void Update ()
+		{
+			CheckForInput ();
+		}
+
+		void CheckForInput ()
+		{
+			if (Input.GetButton ("Fire1") && Time.time > nextFire)
+			{
+				nextFire = Time.time + rate;
+
+				Debug.DrawRay (transform.position, transform.forward, Color.green, range);
+
+				if (Physics.Raycast (transform.position, transform.forward, out hit, range))
+				{
+					if (hit.transform.CompareTag ("Enemy"))
+					{
+						Debug.Log ("Enemy " + hit.transform.name + " hit");
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Welcome.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Welcome : MonoBehaviour {
+
+	public Text text;
+
+	void Start () {
+		text.text = "Hello World";
+		text.gameObject.SetActive (false);
+	}
+}
