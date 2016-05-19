cc969e9f6c7ac529e85d8d14a43b4e7fbf568056
Kendrick Parks <parks.kendrick@gmail.com>
2016-05-18 23:22:45 -0700

    set wall to it's own layer, add trigger to change wall color and wall layer

---
+++ Assets/Scripts/TriggerExample.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace FPSLearning {
+	
+	public class TriggerExample : MonoBehaviour {
+
+		private WalkThroughWall walkThroughWallScript;
+
+		void OnTriggerEnter (Collider other) {
+			Debug.Log (other.name + " has entered");
+			walkThroughWallScript.SetLayerToNotSolid ();
+		}
+
+		void OnTriggerExit (Collider other) {
+			Debug.Log (other.name + " has exited");
+			walkThroughWallScript.SetLayerToDefault ();
+		}
+
+//		void OnTriggerStay (Collider other) {
+//			Debug.Log (other.name + " is in the trigger");
+//		}
+
+		void SetInitialReferences () {
+			if (GameObject.Find ("Wall") != null) {
+				walkThroughWallScript = GameObject.Find ("Wall").GetComponent<WalkThroughWall> ();
+			}
+		}
+
+		void Start () {
+			SetInitialReferences ();
+		}
+	}
+}
