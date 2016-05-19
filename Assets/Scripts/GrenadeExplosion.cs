eef9cd56fd970e174ebd650db263478b5bbbc674
Kendrick Parks <parks.kendrick@gmail.com>
2016-05-19 00:34:05 -0700

    add explosion overlay sphere to detect colliders of objects in a certain radius

--- Assets/Scripts/GrenadeExplosion.cs
+++ Assets/Scripts/GrenadeExplosion.cs
@@ -4,9 +4,20 @@ using System.Collections;
 namespace FPSLearning {
 	public class GrenadeExplosion : MonoBehaviour {
 
+		private Collider[] hitColliders;
+		public float blastRadius;
+
 		void OnCollisionEnter (Collision collision) {
-			Debug.Log (collision.contacts [0].point.ToString ());
+			ExplosionWork (collision.contacts [0].point);
 			Destroy (gameObject);
+		}
+
+		void ExplosionWork (Vector3 explosionPoint) {
+			hitColliders = Physics.OverlapSphere (explosionPoint, blastRadius);
+
+			foreach (Collider hitCollider in hitColliders) {
+				Debug.Log (hitCollider.gameObject.name);
+			}
 		}
 	}
 }
