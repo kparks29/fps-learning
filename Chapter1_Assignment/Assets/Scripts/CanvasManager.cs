a42858e2001a5d0a85146daf996ecee3df4996f5
Kendrick Parks <parks.kendrick@gmail.com>
2016-06-17 22:09:16 -0700

    started assignment,   added welcome message that disappears after 5 seconds, added a trigger on treasure that looks for player tag

---
+++ Chapter1_Assignment/Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace Chapter1_Assignment {
+	public class CanvasManager : MonoBehaviour {
+
+		void Start () {
+			StartCoroutine (DisableCanvas ());
+		}
+
+		IEnumerator DisableCanvas () {
+			yield return new WaitForSeconds (5);
+			gameObject.SetActive (false);
+		}
+
+	}
+}
---
+++ Chapter1_Assignment/Assets/Scripts/TreasureTrigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreasureTrigger : MonoBehaviour {
+
+	void OnTriggerEnter (Collider collider) {
+		if (collider.gameObject.CompareTag ("Player")) {
+			Debug.Log ("Ran into Chest");
+		}
+	}
+}
