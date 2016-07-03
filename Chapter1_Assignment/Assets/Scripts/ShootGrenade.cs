9be15180e75533f152cea80d8ecd493b041da227
Kendrick Parks <parks.kendrick@gmail.com>
2016-07-03 15:16:46 -0700

    add grenade launched

---
+++ Chapter1_Assignment/Assets/Scripts/ShootGrenade.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootGrenade : MonoBehaviour {
+
+	public GameObject grenadePrefab;
+	public float speed = 5;
+	private Transform gunTransform;
+
+	// Use this for initialization
+	void Start () {
+		SetInitialReferences ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckForInput ();
+	}
+
+	void CheckForInput () {
+		if (Input.GetButtonDown ("Fire1")) {
+			GameObject grenade = (GameObject) Instantiate (grenadePrefab, gunTransform.TransformPoint(0, 1.8f, 0), gunTransform.rotation);
+			grenade.GetComponent<Rigidbody>().AddForce (gunTransform.up * speed, ForceMode.Impulse);
+		}
+	}
+
+	void SetInitialReferences () {
+		gunTransform = transform;
+	}
+}
