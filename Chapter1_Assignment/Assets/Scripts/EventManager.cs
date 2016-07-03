7ff7203586ec38ee1bba265cf09db987a63ee5bf
Kendrick Parks <parks.kendrick@gmail.com>
2016-07-02 20:51:31 -0700

    adjust lighting to be darker. add audio clip

--- Chapter1_Assignment/Assets/Scripts/EventManager.cs
+++ Chapter1_Assignment/Assets/Scripts/EventManager.cs
@@ -5,10 +5,31 @@ public class EventManager : MonoBehaviour {
 
 	public delegate void GeneralEvent ();
 	public event GeneralEvent openTreasureEvent;
+	public AudioClip audioClip;
+	private AudioSource audioSource;
+	private Light lighting;
+	private string originalColor;
+	private Color darkColor = new Color ();
 
 	public void CallOpenTreasure () {
 		if (openTreasureEvent != null) {
+			ColorUtility.TryParseHtmlString ("#656146FF", out darkColor);
 			openTreasureEvent ();
+
+			// change color
+			RenderSettings.ambientIntensity = 0;
+			RenderSettings.skybox = null;
+			lighting.color = darkColor;
+
+			// play audio
+			audioSource.time = 11f;
+			audioSource.clip =  audioClip;
+			audioSource.Play ();
 		}
+	}
+
+	void Start () {
+		audioSource = GameObject.Find ("FPSController").GetComponent<AudioSource> ();
+		lighting = GameObject.Find ("Directional light").GetComponent<Light> ();
 	}
 }
