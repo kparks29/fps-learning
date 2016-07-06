4e955baf5aeca22907eed7b2473c1cfdf2f1e29c
Kendrick Parks <parks.kendrick@gmail.com>
2016-07-05 18:50:21 -0700

    add game over script / ui

---
+++ Chapter1/Assets/Scripts/Chapter2/GameManager Scripts/GameManager_GameOver.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Chapter2 {
+	public class GameManager_GameOver : MonoBehaviour {
+
+		private GameManager_Master gameManagerMaster;
+		public GameObject panelGameOver;
+
+		void OnEnable () {
+			SetInitialReferences ();
+			gameManagerMaster.GameOverEvent += TurnOnGameOverPanel;
+		}
+
+		void OnDisable () {
+			gameManagerMaster.GameOverEvent -= TurnOnGameOverPanel;
+		}
+
+		void SetInitialReferences () {
+			gameManagerMaster = GetComponent<GameManager_Master> ();
+		}
+
+		void TurnOnGameOverPanel () {
+			if (panelGameOver != null) {
+				panelGameOver.SetActive (true);
+			} else {
+				Debug.LogWarning ("You forgot to attach a panel to the panelGameOver in the GameManager_GameOver script in the inspector.");
+			}
+		}
+	}
+}
